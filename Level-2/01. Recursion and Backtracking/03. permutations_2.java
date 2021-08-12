457450029c342d5852336c9ec0e669c5e6c313e9
manisha-pawar <manisha.pawar@pepcoding.com>
2021-08-12 23:31:23 +0530

    codes

---
+++ Level-2/01. Recursion and Backtracking/03. permutations_2.java
@@ -0,0 +1,36 @@
+//permutations : boxes on level
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void permutations(int cb, int tb, int[] items, int ssf, int ts, String asf) {
+        if (cb > tb) {
+            if (ssf == ts) {
+                System.out.println(asf);
+            }
+            return;
+        }
+
+        //box -> yes choice
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == 0) {
+                items[i] = 1;
+                permutations(cb + 1, tb, items, ssf + 1, ts, asf + (i + 1));
+                items[i] = 0;
+            }
+        }
+
+        //box -> no choice
+        permutations(cb + 1, tb, items, ssf, ts, asf + 0);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int nboxes = Integer.parseInt(br.readLine());
+        int ritems = Integer.parseInt(br.readLine());
+        permutations(1, nboxes, new int[ritems], 0, ritems, "");
+    }
+
+}
